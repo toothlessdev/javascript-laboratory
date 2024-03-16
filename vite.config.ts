0c100b692317d3fbb79b4fc956ce47aa1f7e5c7a
toothlessdev <toothless042@gmail.com>
2024-03-17 03:35:49 +0900

    config: import alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+  ],
+
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://",
+        changeOrigin: true,
+      },
+    },
+  },
+});
